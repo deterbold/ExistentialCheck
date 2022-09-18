2289316e95cf2348dcdf9c592ae117f359628ce5
Miguel Sicart <deterbold@gmail.com>
2022-09-18 16:42:06 +0200

    Initial Commit

---
+++ ExistentialCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExistentialCheck
+//
+//  Created by Miguel Sicart on 18/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
